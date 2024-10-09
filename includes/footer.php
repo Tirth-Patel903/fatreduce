63b372c6ccd20d6148df737560c5531af791a038
Tirth Patel <99315530+Tirth-Patel903@users.noreply.github.com>
2024-10-09 21:19:49 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,71 @@
+<footer id="footer">
+<div class="footer-top">
+<div class="container">
+<div class="row">
+<div class="col-lg-4 col-md-6">
+<div class="footer-info">
+<img class="ftrlogo" src="assets/img/logo.png">
+<div class="social-links mt-3">
+<a href="https://www.youtube.com/channel/UCTMag_rgAMjoDCWHYX4V6uA" target="_blank" class="youtube"><i class="bx bxl-youtube"></i></a>
+<a href="https://www.linkedin.com/company/imwow" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+<a href="https://www.instagram.com/imwow.in/" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
+</div>
+<p class="mt-4">
+<a class target="_blank" href="https://apps.apple.com/in/app/imwow/id1546994345"><img class="imwowappdwnld-btn img-fluid" src="../b.zmtcdn.com/data/webuikit/9f0c85a5e33adb783fa0aef667075f9e1556003622.png"></a> <a class="mt-3" target="_blank" href="https://play.google.com/store/apps/details?id=com.imwow&amp;hl=en&amp;gl=US"><img class="imwowappdwnld-btn img-fluid" src="../b.zmtcdn.com/data/webuikit/23e930757c3df49840c482a8638bf5c31556001144.png"></a>
+</p>
+</div>
+</div>
+<div class="col-lg-2 col-md-6 footer-links">
+<h4 class="ftr-titl">Sitemap</h4>
+<ul>
+<li><i class="bx bx-chevron-right"></i> <a href="about/about-us.php">About Us</a></li>
+<li><i class="bx bx-chevron-right"></i> <a href="programs/all-programs.html">All Programs</a></li>
+<li><i class="bx bx-chevron-right"></i> <a target="_blank" href="https://forms.gle/6QYCLw88bFjy8D9f7">Career</a></li>
+<li><i class="bx bx-chevron-right"></i> <a href="feedback.html">Feedback</a></li>
+</ul>
+</div>
+<div class="col-lg-3 col-md-6 footer-links">
+<h4 class="ftr-titl">Useful Links</h4>
+<ul>
+<li><i class="bx bx-chevron-right"></i> <a href="privacy-policy.html">Privacy Policy</a></li>
+<li><i class="bx bx-chevron-right"></i> <a href="refund-policy.html">Refund Policy</a></li>
+<li><i class="bx bx-chevron-right"></i> <a href="terms-condition.html">Terms & Conditions</a></li>
+</ul>
+</div>
+<div class="col-lg-3 col-md-6 footer-links">
+<h4 class="ftr-titl">Programs</h4>
+<ul>
+<li><i class="bx bx-chevron-right"></i> <a href="programs/program-detail26d5.html?pid=2">Weight Loss Program</a></li>
+<li><i class="bx bx-chevron-right"></i> <a href="programs/program-detailda41.html?pid=3">Customised Workout Plan</a></li>
+<li><i class="bx bx-chevron-right"></i> <a href="programs/program-detail011e.html?pid=6">Weight Gain Programs</a></li>
+</ul>
+</div>
+
+</div>
+<div class="row">
+<div class="col-md-12">
+<div class="copyright">
+<strong><span>Address:</span></strong> India Accelerator, Level 3 and Level 4, BPTP Centra One, Golf Course Ext Rd, Sector 61, Gurugram, Haryana 122102</br>
+<strong><span>Email:</span></strong> <a href="cdn-cgi/l/email-protection.html" class="__cf_email__" data-cfemail="84d0e1e5e9c4ede9f3ebf3aae7ebaaedea">[email&#160;protected]</a></br>
+<strong><span>Contact No:</span></strong> 9811699450
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-6">
+<div class="copyright">
+&copy; Copyright <strong><span>Imwow Pvt. Ltd.</span></strong>
+All Rights Reserved
+</div>
+</div>
+<div class="col-md-6">
+<div class="copyright">
+Designed By <a target="_blank" href="https://www.hastechnosys.com/">HAS Technosys Pvt. Ltd.</a>
+</div>
+</div>
+</div>
+</div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,66 @@
+<title>IMWOW Weight Loss Program</title>
+<meta content name="descriptison">
+<meta content name="keywords">
+
+
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MLFXF9N');</script>
+
+
+<meta name="robots" Content="Index, follow" />
+<link href="assets/img/favicon.png" rel="icon">
+<link href="assets/img/apple-touch-icon.html" rel="apple-touch-icon">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans&amp;family=Oswald:wght@469&amp;display=swap" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Oswald&amp;display=swap" rel="stylesheet">
+
+<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+<link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+<link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+<link href="assets/vendor/aos/aos.css" rel="stylesheet">
+<link href="../stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+<link href="assets/css/style.css" rel="stylesheet">
+<script src="../kit.fontawesome.com/99878661d4.js" crossorigin="anonymous"></script>
+<script src="../www.google.com/recaptcha/api.js" async defer></script> 
+</head>
+<body>
+
+<header id="header" class="fixed-top">
+<div class="container-fluid d-flex justify-content-end">
+<a href="index.php" class="logo imwowcutm-headr"><img src="assets/img/logo.png"></a>
+<nav class="nav-menu d-none d-lg-block">
+<ul>
+<li class="active"><a href="index.php">Home</a></li>
+<li class="drop-down"><a href="about-us.php">About</a>
+<ul>
+<li><a href="about/about-us.php">About Us</a></li>
+
+</ul>
+</li>
+<li><a href="programs/all-programs.html">All Programs</a></li>
+<li><a href="shop/index.php">Shop</a></li>
+
+
+
+<li><a href="feedback.html">Feedbacks</a></li>
+<li><a href data-toggle="modal" data-target="#imwowcallbckmodal">Get a Call Back</a></li>
+</ul>
+</nav>
+
+<div class="scrollto imwowhdr-social">
+<a target="_blank" href="https://www.youtube.com/channel/UCTMag_rgAMjoDCWHYX4V6uA"><i class="bx bxl-youtube custmscl-hdr"></i></a>
+<a target="_blank" href="https://www.linkedin.com/company/imwow"><i class="bx bxl-linkedin custmscl-hdr"></i></a>
+<a target="_blank" href="https://www.instagram.com/imwow.in/"><i class="bx bxl-instagram custmscl-hdr"></i></a>
+
+</div>
+</div>
+</header>
