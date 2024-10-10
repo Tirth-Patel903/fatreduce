a32494f5669a523f4217ca42143c93d9ccba0988
Tirth Patel <99315530+Tirth-Patel903@users.noreply.github.com>
2024-10-10 10:47:14 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,291 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    
+    <title>Beauty Parlour Management System | About us Page</title>
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="assets/css/style-starter.css">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:400,700,700i&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+    <link rel="stylesheet" href="assets/css/animate.css">
+    
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="assets/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="assets/css/jquery.timepicker.css">
+
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+  </head>
+  <body id="home">
+<?php include_once('includes/header.php');?>
+
+<script src="assets/js/jquery-3.3.1.min.js"></script> <!-- Common jquery plugin -->
+<!--bootstrap working-->
+<script src="assets/js/bootstrap.min.js"></script>
+<!-- //bootstrap working-->
+<!-- disable body scroll which navbar is in active -->
+<script>
+$(function () {
+  $('.navbar-toggler').click(function () {
+    $('body').toggleClass('noscroll');
+  })
+});
+</script>
+<!-- disable body scroll which navbar is in active -->
+
+<!-- breadcrumbs -->
+    <!-- <section class="w3l-inner-banner-main">
+        <div class="about-inner about ">
+            <div class="container">   
+                <div class="main-titles-head text-center">
+                <h3 class="header-name ">
+					About Us
+                </h3>
+            </div>
+</div>
+   </div>
+   <div class="breadcrumbs-sub">
+   <div class="container">   
+    <ul class="breadcrumbs-custom-path">
+        <li class="right-side propClone"><a href="index.php" class="">Home <span class="fa fa-angle-right" aria-hidden="true"></span></a> <p></li>
+        <li class="active ">About</li>
+    </ul>
+</div>
+</div>
+        </div>
+    </section> -->
+    <main id="main">
+<section id>
+<div id="imwowinner-bnr" class="carousel slide" data-ride="carousel">
+<div class="carousel-inner slderfxdhgt">
+<div class="carousel-item active slidergrdnt cutmhmegrdnt">
+<a href="#">
+<img src="../assets/img/about/banner.jpg" class="d-block w-100 img-fluid" alt>
+</a>
+</div>
+</div>
+</div>
+</section>
+
+<section class="custmdnte-bg">
+<div class="container-fluid pl-0">
+<div class="row">
+<div class="col-md-3">
+</div>
+<div class="col-md-9 abutcutmrgt-box">
+<p><b>I’MWOW</b> is an online fitness and nutrition community. Just like many others, I’MWOW
+started
+its journey as a WhatsApp group that used to help people achieve their fitness goals. Today,
+it is
+one of India’s topmost engaging and informative fitness communities with members across
+nations.</p>
+<p><b>I’MWOW</b> was founded in October 2019 by <b>Gunjan Taneja</b>, a fitness influencer and
+enthusiast. In her
+words, she is on a mission to make people believe that fitness can be achieved by anyone.
+Let’s not get
+stuck between the extremes of body positivity and body shaming and focus on what is actually
+good for
+your body. </p>
+<p>“Until I discovered the secrets of Healthy Living, I was the overweight and under-confident
+person who
+could not even wear clothes of her choice. But now, I just don’t feel physically fit but I
+feel more
+confident and Positive than ever! “ </p>
+<p>She has a family of 10 lakh people on <b>YouTube</b> and 3.5 lakh people on Instagram. Her
+videos on YouTube
+have garnered over 29 million views and are viewed in 160 countries. From her own experience
+and
+learnings, she decided to launch this simple and effective “Transformation Program”. She
+along with her
+team has created the fundamental training methodology and is sharing it with the masses
+through this
+platform. The philosophy followed by <b>I’MWOW</b> is to ensure that an individual gets
+transformed not just
+physically but also emotionally, mentally, and psychologically by following a
+well-structured and
+personalized plan. Our body is a mix of so many components and hence, transformation must
+come
+through every component of that mix. </p>
+<p><b>I’MWOW App</b> serves as a one-stop solution for all health & fitness guidance-related
+needs and will take
+you on a joyous ride. Fitness doesn't have to be as difficult as climbing a mountain, it can
+be easy and
+fun. Along with providing you a customized diet and workout guidance, you will also get
+access to fun
+tasks and mindful affirmations, video-guided recipes of interesting cuisines, grocery lists,
+and much
+more</p>
+<p><b>Let's embark on this journey while constantly believing that I’MWOW !!</b></p>
+</div>
+</div>
+</div>
+</section>
+
+
+
+<section class="imwow-wethere-bg">
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-12 pl-140 pr-140" data-aos="fade-up">
+<p class="gunjntitl">We Have <span class="gunjtitllst">Been There</span></p>
+<div class="gunjntitl-brdr"></div>
+</div>
+<div class="col-md-6 mt-100 pl-140 resp-pl0 mobdsply-none" data-aos="fade-right">
+<img class="imwowcmnt img-fluid" src="../assets/img/home/comment.png">
+<p class="imwow-wetherepara">Dt. Bhumika Sharma, a certified professional in Dietetics, Health,
+and Nutrition Education from Mumbai, brings extensive experience to her work. With a proven
+track record in hospitals and a key role at I’MWOW, she's successfully guided over 2000
+clients with an impressive 99% success rate. Bhumika's approach goes beyond traditional diet
+plans, focusing on lasting lifestyle improvements seamlessly integrated into her clients'
+daily lives.</p>
+</div>
+<div class="col-md-6 pr-140 pl-120 resp-pl0 resp-pr0 rsptxt-cntr" data-aos="fade-up">
+<div>
+<img class="img-fluid" src="../assets/img/trainer/am1.png">
+<p class="imwowwethere-name">Bhumika Sharma</p>
+
+</div>
+</div>
+<div class="col-md-6 resp-pl0 dsktpdsply-none" data-aos="fade-right">
+<img class="imwowcmnt img-fluid" src="../assets/img/home/comment.png">
+<p class="imwow-wetherepara">Dt. Bhumika Sharma, a certified professional in Dietetics, Health,
+and Nutrition Education from Mumbai, brings extensive experience to her work. With a proven
+track record in hospitals and a key role at I’MWOW, she's successfully guided over 2000
+clients with an impressive 99% success rate. Bhumika's approach goes beyond traditional diet
+plans, focusing on lasting lifestyle improvements seamlessly integrated into her clients'
+daily lives.</p>
+</div>
+<div class="col-md-6 pl-140 resp-pl0 resp-pr0 rsptxt-cntr">
+<div class="pl-120 resp-pl0">
+<img class="img-fluid" src="../assets/img/trainer/am2.png" data-aos="fade-down">
+<p class="imwowwethere-name">Rajinder Singh</p>
+</div>
+</div>
+<div class="col-md-6 mt-100 pr-140 resp-pr0 mobdsply-none" data-aos="zoom-out">
+<img class="cmntpara2 ml60 img-fluid" src="../assets/img/home/comment.png">
+<p class="imwow-wetherergtpara">Meet Rajinder Singh, a certified fitness expert with over 7
+years of experience in Nutrition and Fitness. Rajinder specialises in crafting personalised
+solutions for fat loss, muscle gain, and overall well-being. He sees fitness as a
+responsibility for a healthy lifestyle, rejecting a one-size-fits-all approach.
+</p>
+</div>
+<div class="col-md-6 dsktpdsply-none" data-aos="zoom-out">
+<img class="cmntpara2 img-fluid" src="../assets/img/home/comment.png">
+<p class="imwow-wetherergtpara">Meet Rajinder Singh, a certified fitness expert with over 7
+years of experience in Nutrition and Fitness. Rajinder specialises in crafting personalised
+solutions for fat loss, muscle gain, and overall well-being. He sees fitness as a
+responsibility for a healthy lifestyle, rejecting a one-size-fits-all approach.
+</p>
+</div>
+<div class="col-md-6 mt-100 pl-140 mobdsply-none" data-aos="zoom-in">
+<img class="imwowcmnt img-fluid" src="../assets/img/home/comment.png">
+<p class="imwow-wetherepara">Dt. Reetal Godhwani, a distinguished nutritionist from India with
+an M.Sc. degree and a Diploma in Food and Nutrition. With over five years of experience, she
+has positively impacted the lives of over a thousand clients, guiding them toward their
+health goals. Unlike the "food police" stereotype, Dt. Godhwani's approach is enlightening
+and empowering.</p>
+</div>
+<div class="col-md-6 pr-140 pl-120 resp-pl0 resp-pr0 rsptxt-cntr">
+<div>
+<img class="img-fluid" src="../assets/img/trainer/am3.png" data-aos="fade-left">
+<p class="imwowwethere-name">Reetal Godhwani</p>
+
+</div>
+</div>
+<div class="col-md-6 dsktpdsply-none" data-aos="zoom-in">
+<img class="imwowcmnt img-fluid" src="../assets/img/home/comment.png">
+<p class="imwow-wetherepara">Dt. Reetal Godhwani, a distinguished nutritionist from India with
+an M.Sc. degree and a Diploma in Food and Nutrition. With over five years of experience, she
+has positively impacted the lives of over a thousand clients, guiding them toward their
+health goals. Unlike the "food police" stereotype, Dt. Godhwani's approach is enlightening
+and empowering.</p>
+</div>
+<div class="col-md-12 imwow-centrline">
+<img class="img-fluid" src="../assets/img/home/center-line.png">
+</div>
+<div class="col-md-12 text-center mt-5">
+<p><a class="allteam-btn" href="../imwow-trainer.html">View More</a></p>
+</div>
+</div>
+</div>
+</section>
+
+
+
+<section id="imwowappdwnldid" class="imwow-realmotvtn-bg">
+<div class="container">
+<div class="row">
+<div class="col-md-4">
+<img class="img-fluid" style="width:300px;" src="../assets/img/home/m-app.png">
+</div>
+<div class="col-md-8 pt-50 aos-init aos-animate" data-aos="zoom-in">
+<p class="gunjntitl">Get the <span class="gunjtitllst">I’mwow App</span></p>
+<div class="realmotvtntitl-brdr"></div>
+<p class="imwow-gunjnpara mt-4">We will send you a link, open it on your phone to download the
+app</p>
+<form class="mt-5" action method="post">
+<div class="row">
+<div class="col-md-5 col-sm-5 col-6">
+<input name="shremail" id="shremail" class="form-control" placeholder="Email" type="text">
+</div>
+<div class="col-md-7 col-sm-7 col-6">
+<input type="submit" class="appshre-btn" id="submit" name="submit">
+</div>
+</div>
+</form>
+<p class="imwow-gunjnpara mt-4">Download App From</p>
+<p></p>
+<div class="row">
+<div class="col-md-3 col-sm-6 col-6">
+<a href="https://apps.apple.com/in/app/imwow/id1546994345" target="_blank"><img class="img-fluid" src="../../b.zmtcdn.com/data/webuikit/9f0c85a5e33adb783fa0aef667075f9e1556003622.png"></a>
+</div>
+<div class="col-md-3 col-sm-6 col-6">
+<a href="https://play.google.com/store/apps/details?id=com.imwow&amp;hl=en&amp;gl=US" target="_blank"><img class="img-fluid" src="../../b.zmtcdn.com/data/webuikit/23e930757c3df49840c482a8638bf5c31556001144.png"></a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+<div class="col-md-12 mt-3">
+</div>
+
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MLFXF9N"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+ 
+</main>
+
+    <?php include_once('includes/footer.php');?>
+<script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/jquery.easing.1.3.js"></script>
+  <script src="assets/js/jquery.waypoints.min.js"></script>
+  <script src="assets/js/jquery.stellar.min.js"></script>
+  <script src="assets/js/jquery.animateNumber.min.js"></script>
+  <script src="assets/js/bootstrap-datepicker.js"></script>
+  <script src="assets/js/jquery.timepicker.min.js"></script>
+  <script src="assets/js/owl.carousel.min.js"></script>
+  <script src="assets/js/jquery.magnific-popup.min.js"></script>
+  <script src="assets/js/scrollax.min.js"></script>
+  <script src="assets/https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="assets/js/google-map.js"></script>
+  <script src="assets/js/main.js"></script>
+
+</html>
